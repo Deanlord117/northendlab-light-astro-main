bb3f58b3c69e7621c3a9b5f1fbaab0736ccdac33
Adrian Rodriguez Arzola <ddadrian.900@gmail.com>
2025-05-25 08:29:58 -0600

    listo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,14 @@
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 import AutoImport from "astro-auto-import";
-import { defineConfig } from "astro/config";
 import remarkCollapse from "remark-collapse";
 import remarkToc from "remark-toc";
 import config from "./src/config/config.json";
-
-let highlighter;
-async function getHighlighter() {
-  if (!highlighter) {
-    const { getHighlighter } = await import("shiki");
-    highlighter = await getHighlighter({ theme: "one-dark-pro" });
-  }
-  return highlighter;
-}
+import { getHighlighter } from "shiki"; // Importación estática
 
 // https://astro.build/config
 export default defineConfig({
@@ -52,6 +45,8 @@ export default defineConfig({
       wrap: true,
     },
     extendDefaultPlugins: true,
-    highlighter: getHighlighter,
+    highlighter: getHighlighter, // Uso de Shiki corregido
   },
+  output: "server", // Configuración necesaria para Vercel
+  adapter: vercel(), // Adaptador de Vercel para despliegue
 });
